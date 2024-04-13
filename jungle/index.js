71351bcde39283f8f007eb4d14391bfc2618785d
gideon <gideonmunene0@gmail.com>
2024-04-13 10:55:09 -0400

    changes

--- jungle/index.js
+++ jungle/index.js
@@ -1,11 +1,35 @@
-function getWildlife(){
-    return fetch("http://localhost:3000/Wildlife", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }
-    })
-    .then(res => res.json())
-    .then(Wildlife => Wildlife)
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const cardContainer = document.getElementById("card-container");
+  
+    fetch("http://localhost:3000/Wildlife") 
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(animal => {
+                const card = createCard(animal);
+                cardContainer.appendChild(card);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
+  
+    function createCard(animal) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+  
+        const nameElement = document.createElement("h2");
+        nameElement.textContent = animal.name;
+  
+        const animal_kingdomElement = document.createElement("p");
+        animal_kingdomElement.textContent = animal.animal_kingdom;
+  
+        const button = document.createElement("button");
+          button.textContent = "Learn More";
+  
+        card.appendChild(nameElement);
+        card.appendChild(animal_kingdomElement);
+        card.appendChild(button);
+  
+        return card;
+    }
+  });
